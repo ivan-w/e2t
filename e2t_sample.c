b35a34d785f812a735c76a3c341dc70318052898
Ivan Warren <ivan@vmfacility.fr>
2019-01-20 10:13:01 +0100

    Sun Jan 20 10:13:01 CET 2019

--- e2t_sample.c
+++ e2t_sample.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-/// static __thread volatile int	foo=1;
+static __thread volatile int	foo=1;
+static __thread volatile int	bar;
 // char	bar[256];
 #define PRIME_TYPE	uint32_t
 #define _STATIC static
@@ -72,7 +73,8 @@ int	main(int ac,char **av)
 	PRIME_TYPE	x,y;
 	PRIME_TYPE	*a;
     // memset(foo,0,sizeof(foo));
-    // printf("%d\n",foo);
+    printf("%d\n",foo);
+    printf("%d\n",bar);
 	// memset(bar,0,sizeof(bar));
 	if(ac<2) return 1;
 	x=strtol(av[1],&eptr,10);
